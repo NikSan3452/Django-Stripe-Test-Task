a4cc047521d9d106cb62a2274c9adbeec9353c71
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-17 11:50:41 +0500

    Добавлены валидаторы, изменен тип поля price

--- payments/models.py
+++ payments/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.core import validators
 
 
 class Item(models.Model):
     name = models.CharField(max_length=250, verbose_name="Наименование товара")
     description = models.TextField(max_length=5000, blank=True, verbose_name="Описание")
-    price = models.FloatField(max_length=20, verbose_name="Цена")
+    price = models.IntegerField(
+        max_length=20,
+        verbose_name="Цена",
+        validators=[
+            validators.MinValueValidator(50),
+            validators.MaxValueValidator(10000000),
+        ],
+    )
 
     class Meta:
-        verbose_name = "Item"
-        verbose_name_plural = "Items"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
 
     def convert_to_usd(self):
-        return f"{self.price:.2f}"
+        return "{0:.2f}".format(self.price / 100)
 
     def __str__(self):
         return self.name
