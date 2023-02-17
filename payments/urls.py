7776632993a4bb46608ed41217696b3e78c09a02
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-17 09:52:31 +0500

    Добавлено представление страницы Item

--- payments/urls.py
+++ payments/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("item/<int:pk>", views.ItemPageView.as_view(), name="item"),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.views.generic.base import TemplateView
+from .models import Item
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        items = Item.objects.all()
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context.update(
+            {
+                "items": items,
+            }
+        )
+        return context
+
+
+class ItemPageView(TemplateView):
+    template_name = "item.html"
+
+    def get_context_data(self, **kwargs):
+        pk = self.kwargs.get("pk")
+        item = Item.objects.get(pk=pk)
+        context = super(ItemPageView, self).get_context_data(**kwargs)
+        context.update(
+            {"item": item, "STRIPE_PUBLISHABLE_KEY": settings.STRIPE_PUBLISHABLE_KEY}
+        )
+        return context
