a5ad22e6ef86c9eb45d480855a3502da28e33436
Nik.San.3452 <nik.san.3452@gmail.com>
2023-02-17 09:48:15 +0500

    Добавлена модель Item

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=250, verbose_name="Наименование товара")
+    description = models.TextField(max_length=5000, blank=True, verbose_name="Описание")
+    price = models.FloatField(max_length=20, verbose_name="Цена")
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+
+    def convert_to_usd(self):
+        return f"{self.price:.2f}"
+
+    def __str__(self):
+        return self.name
